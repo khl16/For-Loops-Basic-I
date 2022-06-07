4003bdfe5ae57f1bf7304ff5ebeeb03cebe0a8b0
khalid <khleadini@gmail.com>
2022-06-08 02:48:17 +0300

    For Loops: Basic I

---
+++ For_Ioop.py
@@ -0,0 +1,31 @@
+1.
+for x in range(0 , 150, 1):
+    print(x)
+2.
+for x in range(5 , 1000, 5):
+    print(x)
+3.
+for X in range(0,100,1): # Numbers between 0 and 100
+    if X % 5 == 0:
+        print("Coding", X)
+    if i % 10 == 0:
+        print("Coding Dojo")
+
+4.
+sum =0
+for B in range(1,500000): 
+    if B%2!=0:
+         sum = sum + B  
+         print(sum) 
+
+5.
+for X in range(2018,1,-4):
+    print(X)
+
+
+lowNum =2
+highNum =9
+mult =3
+for X in range (lowNum, highNum ):
+        if X % mult == 0:
+            print (X)
